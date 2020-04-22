076a591b6ad65d0d2c7a31b5736734f7f84c8bf7
andrew <andrew081791@gmail.com>
2020-04-22 14:27:25 +0300

    bootstrap and jobs are showing up

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Blog(models.Model):
-	title = models.CharField(max_length=200)
+	title = models.CharField(max_length=255)
 	pub_date = models.DateTimeField(null='True')
 	body = models.TextField(null='True')
 	image = models.ImageField(upload_to='images/')
