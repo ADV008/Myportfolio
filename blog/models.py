019665f348c5efb84690b707f6653020cefcb610
andrew <andrew081791@gmail.com>
2020-05-11 15:10:51 +0300

    allblogs turn to youtube

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 
-
 class Blog(models.Model):
 	title = models.CharField(max_length=255)
 	pub_date = models.DateTimeField(null='True')
 	body = models.TextField(null='True')
 	image = models.ImageField(upload_to='images/')
+
 
 	def __str__(self):
 		return self.title
